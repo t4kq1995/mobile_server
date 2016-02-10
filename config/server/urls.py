ef10ba9c59458717919b77496ee355c8a7401f73
t4kq1995 <vlad13zp@mail.ru>
2016-02-10 11:05:04 +0200

    Inital django rest framework

--- config/server/urls.py
+++ config/server/urls.py
@@ -1,9 +1,15 @@
 """
 server URL Configuration
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from server import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^$', views.show_start_page, name='login'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- config/server/views.py
+++ config/server/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from server.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
 
 
-def show_start_page(request):
-    return HttpResponse('Hello')
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
